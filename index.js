4ea2a8fca2b61c2b6409719f89d458312d4cc6db
盧瑞山 <luruishan@luruishande-MacBook-Pro.local>
2019-02-20 15:15:01 +0800

    fixed the threshold

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function init(exchange1, exchange2, market) {
         let e2Brick = Math.round(((e1Bid0 - e2Ask0) / e2Ask0) * 1000000) / 10000
         let e1Brick = Math.round(((e2Bid0 - e1Ask0) / e1Ask0) * 1000000) / 10000
         console.log(e2Brick + '%', e1Brick + '%')
-        if (e2Brick > 0.02 || e1Brick > 0.02) {
+        if (e2Brick > 0.01 || e1Brick > 0.01) {
             let exchangepair = exchange1 + '/' + exchange2
             count[exchangepair] = count[exchangepair] ? count[exchangepair] + 1 : 1
         }
@@ -30,22 +30,22 @@ let moveBrickFunc1 = init('binance', 'newtonxchange', 'BTC/USDT')
 let timer1 = setInterval(moveBrickFunc1, 1000)
 setTimeout(() => {
     clearInterval(timer1)
-    console.log('binance/newtonxchange 可獲利機會', count['binance/newtonxchange'] || 0)
-}, 60000)
+    console.log('binance/newtonxchange 可獲利機會', count['binance/newtonxchange'] || 0,'次')
+}, 30000)
 
 let moveBrickFunc2 = init('newtonxchange', 'gateio', 'BTC/USDT')
 
 let timer2 = setInterval(moveBrickFunc2, 1000)
 setTimeout(() => {
     clearInterval(timer2)
-    console.log('newtonxchange/gateio 可獲利機會', count['newtonxchange/gateio'] || 0)
-}, 60000)
+    console.log('newtonxchange/gateio 可獲利機會', count['newtonxchange/gateio'] || 0,'次')
+}, 30000)
 
 let moveBrickFunc3 = init('newtonxchange', 'exx', 'BTC/USDT')
 
 let timer3 = setInterval(moveBrickFunc3, 1000)
 setTimeout(() => {
     clearInterval(timer3)
-    console.log('newtonxchange/exx 可獲利機會', count['newtonxchange/exx'] || 0)
-}, 60000)
+    console.log('newtonxchange/exx 可獲利機會', count['newtonxchange/exx'] || 0,'次')
+}, 30000)
 
